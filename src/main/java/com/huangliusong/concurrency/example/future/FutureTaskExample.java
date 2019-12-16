bf89703464aaeeb2d2164f9ae96f07808debecd8
huangliusong1994 <huangliusong1994@gmail.com>
2019-12-16 15:04:22 +0800

    futureTask

---
+++ src/main/java/com/huangliusong/concurrency/example/future/FutureTaskExample.java
@@ -0,0 +1,35 @@
+package com.huangliusong.concurrency.example.future;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author huangliusong
+ * @since 2019/12/16
+ * {@link }
+ */
+@Slf4j
+public class FutureTaskExample {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        FutureTask<String> futureTask = new FutureTask<>(new MyCallable());
+        new Thread(futureTask).start();
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        log.info("result：{}", result);
+    }
+
+    static class MyCallable implements Callable<String> {
+
+        @Override
+        public String call() throws Exception {
+            log.info("do something in callable");
+            Thread.sleep(5000);
+            return "Done";
+        }
+    }
+}
